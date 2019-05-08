f8c22f5307c5810e483bcf674559024caa16211f
sonalig1 <49108933+sonalig1@users.noreply.github.com>
2019-05-08 14:23:42 -0400

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -7,45 +7,34 @@
 <title> Create Post </title>
 <link rel="stylesheet" type="text/css" href="feedbackphp.css" />
 </head>
+
 <body>
   <h1> Customers Feedbacks <h1>
     <?php
     #connect to mysql database
     $db = mysqli_connect("studentdb-maria.gl.umbc.edu","sarge1","sarge1","sarge1");
-
     if (mysqli_connect_errno())
     exit("Error - could not connect to MySQL");
-
     #get the parameter from the HTML form that this PHP program is connected to
     #since data from the form is sent by the HTTP POST action, use the $_POST array here
-
    $feedbackname = $_POST['name'];
     $feedbackcomment = $_POST['comment'];
+
+
      ?>
 
     <?php
-
     $constructed_query = "INSERT INTO feedback (name, comment) values ('$feedbackname', '$feedbackcomment')";
-
     $result = mysqli_query($db, $constructed_query);
-
     ?>
 
     <?php
     #construct a query
     #query should look like this:
-
-
-
     $constructed_query = "SELECT * FROM feedback";
-
     #sanity check: print query to see if constructued query is correct
-
     #Execute query
     $result = mysqli_query($db, $constructed_query);
-
-
-
     #if result object is not returned, then print an error and exit the PHP program
     if(! $result){
     print("Error - query could not be executed");
@@ -55,13 +44,9 @@
     }
     ?>
 
-      <?php
+      <?php
      $num_rows = mysqli_num_rows($result);
-
-
      $num_fields = mysqli_num_fields($result);
-
-
     ?>
 
 
